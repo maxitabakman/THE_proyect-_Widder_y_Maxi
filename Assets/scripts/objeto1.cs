9913000a42a62fc4ad756775d300fc0e6a07b17f
maxitabakman <49928297@est.ort.edu.ar>
2026-05-29 14:33:40 -0300

    Initial commit

---
+++ Assets/scripts/objeto1.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class objeto1 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    void OnCollisionEnter(Collision col){ 
+    
+        if(col.gameObject.CompareTag("objeto")){ 
+        Debug.Log("hubo colision con un pickable");
+        Destroy(col.gameObject);}}
+
+}
